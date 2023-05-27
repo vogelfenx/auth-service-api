99532ba141e0ffa013a90250bf41bc05f6ace3d6
Eugene Astakhov <evgeneration01@gmail.com>
2023-05-27 12:47:43 +0300

    Add jwt template

--- app/src/api/v1/service/routes.py
+++ app/src/api/v1/service/routes.py
@@ -1,12 +1,25 @@
+from datetime import timedelta
 from http import HTTPStatus
 from typing import Annotated
 from uuid import UUID
+from fastapi.security import OAuth2PasswordRequestForm
 
-from fastapi import APIRouter, Depends, HTTPException, Path, Query
+from fastapi import APIRouter, Depends, HTTPException, Path, Query, status
+
+from security.token.jwt import (
+    Token,
+    User,
+    authenticate_user,
+    create_access_token,
+    fake_users_db,
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+    get_current_active_user,
+)
 
 router = APIRouter()
 
 
+# Igor
 @router.post("/signin")
 async def signin(
     user_id: Annotated[str, Query(description="A user id.")],
@@ -41,6 +54,7 @@ async def logout():
     return {"message": "This is logout!"}
 
 
+# Igor
 @router.put("/user")
 async def edit_user(
     login: Annotated[str, Query(description="A user login.")],
@@ -56,3 +70,38 @@ async def get_user_history():
     """Get user`s history by id and token."""
 
     return {"message": "This is a history!"}
+
+
+##### Test jwt example
+@router.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = authenticate_user(
+        fake_users_db, form_data.username, form_data.password
+    )
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/users/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return current_user
+
+
+@router.get("/users/me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
