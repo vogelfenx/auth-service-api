9006fbf0d37ea6745144c93b2b3cf84afe22e932
Eugene Astakhov <evgeneration01@gmail.com>
2023-05-31 11:00:12 +0300

    config settings

--- app/src/security/token/jwt.py
+++ app/src/security/token/jwt.py
@@ -10,16 +10,12 @@ from pydantic import BaseModel
 from src.db.storage.auth_db import PgConnector
 
 from core.logger import get_logger
+from core.config import security_settings
 
 logger = get_logger(__name__)
 # to get a string like this run:
 # openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
 
-# TODO Перенести в settings
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 30
 
 fake_users_db = {
     "admin": {
@@ -100,12 +96,20 @@ def create_token(data: dict, expires_delta: timedelta | None = None):
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(
+        claims=to_encode,
+        key=security_settings.SECRET_KEY,
+        algorithm=security_settings.ALGORITHM,
+    )
     return encoded_jwt
 
 
 def decode_token(token: str):
-    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    payload = jwt.decode(
+        token=token,
+        key=security_settings.SECRET_KEY,
+        algorithms=[security_settings.ALGORITHM],
+    )
 
     exp = payload.get("exp")
     if not exp:
@@ -125,7 +129,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = decode_token(token)
         username: str | None = payload.get("sub")
         if username is None:
             raise credentials_exception
