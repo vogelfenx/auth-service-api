50f1050e30f0251add1b2141cc4b764d87df652d
Eugene Astakhov <evgeneration01@gmail.com>
2023-05-31 09:31:23 +0300

    Prev changes

--- app/src/api/v1/service/routes.py
+++ app/src/api/v1/service/routes.py
@@ -2,20 +2,36 @@ from datetime import timedelta
 from http import HTTPStatus
 from typing import Annotated
 from uuid import UUID
+
+from fastapi import (
+    APIRouter,
+    Depends,
+    HTTPException,
+    Path,
+    Query,
+    Request,
+    status,
+)
 from fastapi.security import OAuth2PasswordRequestForm
-
-from fastapi import APIRouter, Depends, HTTPException, Path, Query, status
-
 from security.token.jwt import (
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+    CREDENTIALS_EXCEPTION,
+    REFRESH_TOKEN_EXPIRE_MINUTES,
     Token,
     User,
+    add_blacklist_token,
     authenticate_user,
-    create_access_token,
+    create_token,
+    decode_token,
     fake_users_db,
-    ACCESS_TOKEN_EXPIRE_MINUTES,
     get_current_active_user,
+    get_current_user_token,
 )
+from core.logger import get_logger
 
+
+logger = get_logger(__name__)
+logger.setLevel(level="DEBUG")
 router = APIRouter()
 
 
@@ -23,56 +39,13 @@ router = APIRouter()
 @router.post("/signin")
 async def signin(
     user_id: Annotated[str, Query(description="A user id.")],
-    psw: Annotated[str, Query(description="User`s password.")],
+    psw: Annotated[str, Query(description="User password.")],
 ):
     """Registration a user."""
 
     return {"message": "This is signin!"}
 
 
-@router.get("/login")
-async def login(
-    user_id: Annotated[str, Query(description="A user login.")],
-    psw: Annotated[str, Query(description="User`s password.")],
-):
-    """Return two jwt tokens, if user is registred."""
-
-    return {"message": "This is login!"}
-
-
-@router.put("/update")
-async def update_token():
-    """Return two jwt tokens, if user is registred."""
-
-    return {"message": "This is update_token!"}
-
-
-@router.get("/logout")
-async def logout():
-    """Logout also delete jwt token."""
-
-    return {"message": "This is logout!"}
-
-
-# Igor
-@router.put("/user")
-async def edit_user(
-    login: Annotated[str, Query(description="A user login.")],
-    psw: Annotated[str, Query(description="User`s hashed password.")],
-):
-    """Change login or password for user by id."""
-
-    return {"message": "This is a user editor!"}
-
-
-@router.get("/user/history")
-async def get_user_history():
-    """Get user`s history by id and token."""
-
-    return {"message": "This is a history!"}
-
-
-##### Test jwt example
 @router.post("/token", response_model=Token)
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
@@ -87,10 +60,75 @@ async def login_for_access_token(
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
+    refresh_token_expires = timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
+
+    access_token = create_token(
+        data={"sub": user.username},
+        expires_delta=access_token_expires,
     )
+    refresh_token = create_token(
+        data={"sub": user.username},
+        expires_delta=refresh_token_expires,
+    )
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "token_type": "bearer",
+    }
+
+
+@router.put("/update", response_model=Token)
+async def update_token(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+    current_token: Annotated[User, Depends(get_current_user_token)],
+):
+    """Return two jwt tokens, if user is registred."""
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_token(
+        data={"sub": current_user.username},
+        expires_delta=access_token_expires,
+    )
+
+    # TODO: старый токен следует записать в cache
+
+    logger.debug("old_token: {0}".format(current_token))
+    logger.debug("new_token: {0}".format(access_token))
+
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/logout")
+async def logout(token: str = Depends(get_current_user_token)):
+    """Logout also delete jwt token."""
+
+    # TODO попробовать использовать тут протокол
+    if add_blacklist_token(token):
+        # TODO Переделать на orjson
+        return status.HTTP_200_OK
+    return status.HTTP_500_INTERNAL_SERVER_ERROR
+
+
+# TODO добавить ручку change_pass (должен производить логаут)
+# Igor
+@router.put("/user")
+async def edit_user(
+    login: Annotated[str, Query(description="A user login.")],
+    psw: Annotated[str, Query(description="User hashed password.")],
+):
+    """Change login or password for user by id."""
+
+    return {"message": "This is a user editor!"}
+
+
+@router.get("/user/history")
+async def get_user_history():
+    """Get user history by id and token."""
+
+    return {"message": "This is a history!"}
+
+
+##### Test jwt example
 
 
 @router.get("/users/me/", response_model=User)
@@ -105,3 +143,27 @@ async def read_own_items(
     current_user: Annotated[User, Depends(get_current_active_user)]
 ):
     return [{"item_id": "Foo", "owner": current_user.username}]
+
+
+@router.post("/refresh")
+async def refresh(request: Request):
+    try:
+        req = await request.json()
+        if req.get("grant_type") != "refresh_token":
+            raise ValueError("Not refresh token")
+
+        token = req.get("refresh_token")
+        payload = decode_token(token)
+
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_token(
+            data={"sub": payload.get("sub")},
+            expires_delta=access_token_expires,
+        )
+
+        logger.debug("refresh_token: {0}".format(token))
+        logger.debug("access_token: {0}".format(access_token))
+        return {"access_token": access_token, "token_type": "bearer"}
+
+    except Exception as e:
+        raise CREDENTIALS_EXCEPTION
--- app/src/security/token/jwt.py
+++ app/src/security/token/jwt.py
@@ -9,26 +9,39 @@ from pydantic import BaseModel
 
 from src.db.storage.auth_db import PgConnector
 
+from core.logger import get_logger
+
+logger = get_logger(__name__)
 # to get a string like this run:
 # openssl rand -hex 32
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+# TODO Перенести в settings
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 30
 
 fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
+    "admin": {
+        "username": "admin",
         "full_name": "John Doe",
         "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$NiCCv0lafj1e87ywEvbaT.Y8Dj09.kUQidDgr3Nsa46jFc7BaoWBG",
+        "hashed_password": "$2b$12$nTivOWaQA.nqj0PgApNpqe3h9kkFTGWvvZz496Uosd50IhaoHd3u.",
         "disabled": False,
     }
 }
 
+# Error
+CREDENTIALS_EXCEPTION = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials",
+    headers={"WWW-Authenticate": "Bearer"},
+)
+
 
 class Token(BaseModel):
     access_token: str
+    refresh_token: str
     token_type: str
 
 
@@ -36,6 +49,7 @@ class TokenData(BaseModel):
     username: str | None = None
 
 
+# should be ORM
 class User(BaseModel):
     username: str
     email: str | None = None
@@ -54,6 +68,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="v1/service/token")
 
 app = FastAPI()
+
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
@@ -78,7 +93,7 @@ def authenticate_user(fake_db, username: str, password: str):
     return user
 
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
+def create_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
@@ -87,6 +102,20 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+def decode_token(token: str):
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+    exp = payload.get("exp")
+    if not exp:
+        raise ValueError("Wrong payload")
+
+    # Check if token is not expired
+    if datetime.utcfromtimestamp(exp) < datetime.utcnow():
+        raise ValueError("Refresh token expired")
+
+    return payload
 
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
@@ -103,10 +132,17 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
+    # TODO возможно стоит не ходить в базу повторно
     user = get_user(fake_users_db, username=token_data.username)
     if user is None:
         raise credentials_exception
     return user
+
+
+async def get_current_user_token(
+    token: Annotated[str, Depends(oauth2_scheme)]
+):
+    return token
 
 
 async def get_current_active_user(
@@ -115,3 +151,9 @@ async def get_current_active_user(
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
+
+
+async def add_blacklist_token(token):
+    logger.error("This is dummy function")
+
+    return True
