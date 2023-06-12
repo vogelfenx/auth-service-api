41cc57f88ed758bb934dae15c4fe94e37c912821
Eugene Astakhov <evgeneration01@gmail.com>
2023-06-12 20:02:38 +0300

    fix imports

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -1,38 +1,23 @@
 from logging.config import fileConfig
 
-from sqlalchemy import create_engine
-
 from alembic import context
-
+from sqlalchemy import create_engine
 from src.core.config import postgres_settings as pg_conf
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = None  #  type: ignore
 URL = "{driver}://{user}:{password}@{host}:{port}/{database}".format(
-            driver="postgresql+psycopg2",
-            user=pg_conf.POSTGRES_USER,
-            password=pg_conf.POSTGRES_PASSWORD,
-            host=pg_conf.POSTGRES_HOST,
-            port=pg_conf.POSTGRES_PORT,
-            database=pg_conf.POSTGRES_DB,
-        )
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+    driver="postgresql+psycopg2",
+    user=pg_conf.POSTGRES_USER,
+    password=pg_conf.POSTGRES_PASSWORD,
+    host=pg_conf.POSTGRES_HOST,
+    port=pg_conf.POSTGRES_PORT,
+    database=pg_conf.POSTGRES_DB,
+)
 
 
 def run_migrations_offline() -> None:
